afd1d91678532d09b78d230d03e8dde44756c2e8
lewis810k <alsdl901@gmail.com>
2017-03-17 04:25:29 +0900

    post list API 구현

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.post_list, name='list'),
     url(r'^create/$', views.post_create, name='create'),
     url(r'^photo/add/$', views.post_photo_add, name='photo_add'),
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,11 +1,40 @@
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from post.models import Post, PostPhoto
 
 User = get_user_model()
+
+
+def post_list(request):
+    post_list = Post.objects.select_related('author')
+    post_dict_list = []
+    for post in post_list:
+        cur_post_photo_list = post.postphoto_set.all()
+        cur_post_photo_dict_list = []
+        for post_photo in cur_post_photo_list:
+            cur_post_photo_dict = {
+                'pk': post_photo.pk,
+                'photo': post_photo.photo.url,
+            }
+            cur_post_photo_dict_list.append(cur_post_photo_dict)
+        cur_post_dict = {
+            'pk': post.pk,
+            'created_date': post.created_date,
+            'photo_list': cur_post_photo_dict_list,
+            'author': {
+                'pk': post.author.pk,
+                'username': post.author.username,
+            },
+        }
+        post_dict_list.append(cur_post_dict)
+    context = {
+        'post_list': post_dict_list,
+    }
+    return JsonResponse(data=context)
 
 
 @csrf_exempt
