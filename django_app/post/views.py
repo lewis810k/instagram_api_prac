d60be2dbe03c013489a26cc08dc8e606c6de5a50
lewis810k <alsdl901@gmail.com>
2017-03-17 01:19:03 +0900

    Insta API - post 앱의 post_create 뷰 생성 및 관련 url 설정

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from post.models import Post
+
+User = get_user_model()
+
+
+@csrf_exempt
+def post_create(request):
+    if request.method == 'POST':
+        try:
+            author_id = request.POST['author_id']
+            author = User.objects.get(id=author_id)
+        except KeyError as e:
+            return HttpResponse('key "author_id" is required')
+        except User.DoesNotExist:
+            return HttpResponse('author_id {} does not exist'.format(
+                request.POST['author_id']
+            ))
+
+        post = Post.objects.create(author=author)
+        return HttpResponse('{}'.format(post.pk))
+    else:
+        return HttpResponse('Post create view')
