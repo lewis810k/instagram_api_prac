f7101f533e26fa6cdacc3cfa74fcb5f1397ef674
lewis810k <alsdl901@gmail.com>
2017-03-17 01:47:42 +0900

    Insta API - post admin에 모델 추가

--- django_app/post/admin.py
+++ django_app/post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from post.models import Post
+from post.models import PostPhoto
+
+admin.site.register(Post)
+admin.site.register(PostPhoto)
